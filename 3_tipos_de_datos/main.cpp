14737ab0293348194ec69346f6ce431f1d3ac280
jeremias256 <jere.menacho@gmail.com>
2022-08-19 20:59:40 -0300

    2C prog

---
+++ 3_tipos_de_datos/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Tipo de datos \n" << endl;
+
+    int entero     = 15;
+    cout << entero << endl ;
+
+    float flotante = 15.20;
+    cout << flotante << endl;
+
+    double mayor = 16.2345;
+    cout << mayor << endl;
+
+    char letra = '1';
+    cout << letra << endl;
+}
---
+++ 4_entrada_datos/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Ingrese un numero: ";
+    cin >> num;
+
+    cout << "\n El numero que digito es: " << num ;
+    return 0;
+}
